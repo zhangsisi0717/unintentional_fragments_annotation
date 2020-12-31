138185c22513ab4cdf3b474c8dff061c7ec8953a
Sisi Zhang <zhangsisi0717@gmail.com>
2020-12-31 17:35:16 -0600

    fix minor bug

--- MoNA_data_filter.py
+++ MoNA_data_filter.py
@@ -66,5 +66,5 @@ for i in tqdm(range(len(mona_raw))):
             temp.append(('precursor type', pre_type))
     mona_spectra[i] = dict(temp)
 
-with open('save_file_name', 'wb') as f:
+with open('file_name.pkl', 'wb') as f:
     pkl.dump(mona_spectra, f)
